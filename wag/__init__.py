dcc1cf6b7ba38d20a9d3a1f1c6a02cff69c89972
rajikaimal <inbox.rajika@gmail.com>
2016-09-07 15:01:41 +0530

    mongo connection

--- wag/__init__.py
+++ wag/__init__.py
@@ -6,7 +6,7 @@ app.config.from_object(__name__)
 SECRET_KEY = '6lsqywSdlF6MYJAoNzbn-DTwaCnEa6jH'
 # MONGODB_HOST = 'mongodb://rajika:intel@123@ds019746.mlab.com:19746/heroku_fqghj6dr'
 # MONGODB_PORT = 19746
-MONGO_URI = 'mongodb://rajika:intel@123@ds019746.mlab.com:19746/heroku_fqghj6dr'
+MONGO_URI = 'mongodb://heroku_fqghj6dr:intel123@ds019746.mlab.com:19746/heroku_fqghj6dr'
 
 app.secret_key = SECRET_KEY
 
