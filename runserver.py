467139ac0974e24cf1e003c9ce41ff42258e9afb
Dishan Rajapaksha <dsrajapaksha@outlook.com>
2016-09-07 22:13:51 +0530

    server url

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from wag import app
 import setup
 print 'Server running'
-app.run(host='127.0.0.1', debug=True)
-
+#app.run(host='127.0.0.1', debug=True)
+app.run(host='0.0.0.0', port=int('5000'), debug=False)
