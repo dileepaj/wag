25f834fbfb562c21af30f2c0d81fc2138f20b3c5
Dishan Rajapaksha <dsrajapaksha@outlook.com>
2016-09-07 15:47:59 +0530

    Mongo

--- wag/__init__.py
+++ wag/__init__.py
@@ -5,13 +5,8 @@ app.config.from_object(__name__)
 # configuration
 SECRET_KEY = '6lsqywSdlF6MYJAoNzbn-DTwaCnEa6jH'
 
-MONGO_HOST='mongodb://<MONGO_USERNAME>:<MONGO_PASSWORD>@ds019746.mlab.com:19746/heroku_fqghj6dr',
-
-MONGO_PORT = '19746'
-MONGO_USERNAME = 'heroku_fqghj6dr'
-MONGO_PASSWORD = 'intel123'
-
-# MONGO_URI = 'mongodb://heroku_fqghj6dr:intel123@ds019746.mlab.com:19746/heroku_fqghj6dr'
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
 
 app.secret_key = SECRET_KEY
 
