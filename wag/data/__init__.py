81ffc80bc332515568e70fd2794e8da1505f1ce1
rajikaimal <inbox.rajika@gmail.com>
2016-09-07 13:57:13 +0530

    connection string

--- wag/data/__init__.py
+++ wag/data/__init__.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # configuration
-SECRET_KEY = 'QqP@t5Boj`B#KAY~*Xs^z4kUGp04^{UDLV-V9%(m(0f4n+I M{7sMG)Zb?f#R7*x'
-MONGODB_HOST = 'localhost'
+SECRET_KEY = '6lsqywSdlF6MYJAoNzbn-DTwaCnEa6jH'
+MONGODB_HOST = 'mongodb://rajika:intel@123@ds019746.mlab.com:19746/heroku_fqghj6dr'
 MONGODB_PORT = 27017
 
 app.secret_key = SECRET_KEY
